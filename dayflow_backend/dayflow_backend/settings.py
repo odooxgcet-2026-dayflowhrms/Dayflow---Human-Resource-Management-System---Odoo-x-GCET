ddf75cc9ce9179a7fc62c92f76afaedba5ab26c3
Anurag Puthiyaveetil Othayoth <anuragpo393@gmail.com>
2026-01-03 17:15:04 +0530

    Update settings.py
    Add static files configuration and update settings.

--- dayflow_backend/dayflow_backend/settings.py
+++ dayflow_backend/dayflow_backend/settings.py
@@ -137,4 +137,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 CORS_ALLOW_ALL_ORIGINS = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
